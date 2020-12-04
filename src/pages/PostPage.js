710cf9f3f107fd0d7e4c2b3e230bbcfc0eb1de91
ratchagarn <ratchagarn@gmail.com>
2020-12-05 00:39:02 +0700

    Add pagination for post page

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,11 +1,24 @@
-import { useQuery } from 'react-query'
+import { usePaginatedQuery } from 'react-query'
 
 import PageLayout from '../components/PageLayout'
 import Posts from '../components/Posts'
 
+import usePageQuery from '../hooks/usePageQuery'
+
 function UserPage() {
-  const { isLoading, error, data } = useQuery('repoData', () =>
-    fetch('https://gorest.co.in/public-api/posts').then((res) => res.json())
+  const {
+    pageQuery: { page },
+  } = usePageQuery()
+
+  let url = 'https://gorest.co.in/public-api/posts'
+
+  if (/\d/.test(page)) {
+    url += '?page=' + page
+  }
+
+  const { isLoading, error, data } = usePaginatedQuery(
+    ['postsData', page],
+    () => fetch(url).then((res) => res.json())
   )
 
   if (error) return 'An error has occurred: ' + error.message
