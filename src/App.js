f6341aef0093ae8398c1e7d853b7ccc9c79b199e
ratchagarn <ratchagarn@gmail.com>
2020-12-06 13:35:09 +0700

    Use hash router instead

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom'
+import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import { QueryCache, ReactQueryCacheProvider } from 'react-query'
 
 import './App.css'
